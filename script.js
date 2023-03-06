5a04343997ff8bb3b5950551f8b4af314e80e41f
Andrii <reyandrey98@gmail.com>
2023-03-06 19:52:02 +0200

    infinity scroll

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const list = document.querySelector(".js-list");
-const load = document.querySelector(".js-load");
+// const load = document.querySelector(".js-load");
+const guard = document.querySelector(".js-guard");
 
 const KEY = "w3kHq_TfVkoA4iwhrcqa";
 const BASE_URL = "https://the-one-api.dev/v2";
+const options = {
+	root: null,
+	rootMargin: "300px",
+	threshhold: 1.0,
+};
+
+const observer = new IntersectionObserver(onInfinityLoad, options);
 
 let page = 1;
 
-load.addEventListener("click", onLoad);
+// load.addEventListener("click", onLoad);
 
-function rengsApi(page = 1) {
+function ringsApi(page = 1) {
 	const options = {
 		headers: {
 			Authorization: `Bearer ${KEY}`,
 		},
 	};
-	const resp = fetch(`${BASE_URL}/character?limit=20&page=${page}`, options)
+	const resp = fetch(`${BASE_URL}/character?limit=50&page=${page}`, options)
 		.then((resp) => {
 			if (!resp.ok) {
 				throw new Error(resp.statusText);
@@ -26,10 +34,11 @@ function rengsApi(page = 1) {
 	return resp;
 }
 
-rengsApi()
+ringsApi()
 	.then((data) => {
 		createMarkup(data.docs);
-		load.hidden = false;
+		// load.hidden = false;
+		observer.observe(guard);
 	})
 	.catch((err) => console.log(err));
 
@@ -46,14 +55,28 @@ function createMarkup(arr) {
 	list.insertAdjacentHTML("beforeend", markup);
 }
 
-function onLoad() {
-	page += 1;
-	rengsApi(page)
-		.then((data) => {
-			createMarkup(data.docs);
-			if (data.page === data.pages) {
-				load.hidden = true;
-			}
-		})
-		.catch((err) => console.log(err));
+// function onLoad() {
+// 	page += 1;
+// 	ringsApi(page)
+// 		.then((data) => {
+// 			createMarkup(data.docs);
+// 			if (data.page === data.pages) {
+// 				load.hidden = true;
+// 			}
+// 		})
+// 		.catch((err) => console.log(err));
+// }
+function onInfinityLoad(entries, observer) {
+	console.log(entries);
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			page += 1;
+			ringsApi(page).then((data) => {
+				createMarkup(data.docs);
+				if (data.page === data.pages) {
+					observer.unobserve(guard);
+				}
+			});
+		}
+	});
 }
